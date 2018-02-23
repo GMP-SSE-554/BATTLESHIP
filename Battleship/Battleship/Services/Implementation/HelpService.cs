1a38c1ff1ebe275586aa93c86224e005ec60406f
ToastyTad <toastytadgit@gmail.com>
2018-02-23 09:43:47 -0500

    Improved help service to read from file.

--- Battleship/Battleship/Services/Implementation/HelpService.cs
+++ Battleship/Battleship/Services/Implementation/HelpService.cs
@@ -1,4 +1,5 @@
 ﻿using Battleship.Models;
+using System.IO;
 
 namespace Battleship.Services
 {
@@ -14,28 +15,10 @@ namespace Battleship.Services
 
             if (instruction != null)
             {
-                instructions += "The game is played as explained in the following steps:\n";
-                instructions += "1. Place your ships in the order they appear on the scoreboard " +
-                    "from top to bottom.\n";
-                instructions += "\ta. Right click to change the orientation of the ship.\n";
-                instructions += "\tb. Left click to place the ship's root tile in specified tile.\n";
-                instructions += "\tc. Currently, once a ship is placed, it can not be replaced.\n";
-                instructions += "2. After all ships are placed, select tiles to try to sink the " +
-                    "opponent's ships before the opponent sinks all of your ships.\n";
-                instructions += "3. Once all ships for one player have been destoryed, the other " +
-                    "player wins the game.\n\n";
-
-                instructions += "Notes on tile colors:\n";
-                instructions += "1. All tiles are initially filled in with Floral White and outlined " +
-                    "with Slate Gray.\n";
-                instructions += "2. Tiles containing player ships that have NOT been hit by the " +
-                    "opponent are filled in with COLOR.\n";
-                instructions += "3. Tiles containing player ships that have been hit by the " +
-                    "opponent are filled in with COLOR.\n";
-                instructions += "4. Tiles selected by the player that do NOT contain enemy ships " +
-                    "are outlined with COLOR.\n";
-                instructions += "5. Tiles selected by the player that do contain enemy ships are " +
-                    "outlined with COLOR.";
+                foreach (var line in File.ReadAllLines("Assets/HelpMessage.txt"))
+                {
+                    instructions += $"{line}\n";
+                }
 
                 instruction.Instructions = instructions;
             }
