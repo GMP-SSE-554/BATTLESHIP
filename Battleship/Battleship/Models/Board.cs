dbbfab22959fe89b8b703df780cf41fcc43badf1
SeditiousSnake <tyson31088@gmail.com>
2018-02-25 23:38:30 -0500

    Revert "Revert "Revert "Revert "Merge branch 'master' into ShipPlacement""""
    This reverts commit 1c68c17218eb07b61e04001d4cc1a70c4ef5b9cc.

--- Battleship/Battleship/Models/Board.cs
+++ Battleship/Battleship/Models/Board.cs
@@ -4,18 +4,11 @@ namespace Battleship.Models
 {
     public class Board
     {
-        int _numRows;
-        public int NumRows
+        bool _isHumanBoard;
+        public bool IsHumanBoard
         {
-            get { return _numRows; }
-            set { _numRows = value; }
-        }
-
-        int _numColumns;
-        public int NumColumns
-        {
-            get { return _numColumns; }
-            set { _numColumns = value; }
+            get { return _isHumanBoard; }
+            set { _isHumanBoard = value; }
         }
 
         public List<Tile> Tiles = new List<Tile>();
